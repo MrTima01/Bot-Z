bdb21e5edf79e9d5dbb048f6d9176aa46ada706e
MrTima01 <106863228+MrTima01@users.noreply.github.com>
2022-06-05 06:32:43 -0300

    Update AMENU.js

--- plugins/AMENU.js
+++ plugins/AMENU.js
@@ -1,50 +1,358 @@
-const Amazon = require('../events');
-const Config = require('../config');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
-const axios = require('axios');
-const hrs = new Date().getHours({ timeZone: Config.TIME_ZONE })
-const Language = require('../language');
-const Lang = Language.getString('amazone');
-const tk = Config.WORKTYPE == 'public' ? false : true
+let levelling = require('../lib/levelling')
+let { MessageType } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let path = require('path')
+let fetch = require('node-fetch')
+let moment = require('moment-timezone')
+const defaultMenu = {
+    before: `╭────────────────╮
+          ✪  *DARK MAX* ✪           
+╰────────────────╯
+       
+  `.trimStart(),
+  header: '┌─〔 %category 〕',
+  body: '├ %cmd',
+  footer: '└────\n',
+  after: `
+`,
+}
+let handler = async (m, { conn, usedPrefix: _p, args, command }) => {
+  let tags
+  let teks = `${args[0]}`.toLowerCase()
+  let arrayMenu = ['git', 'game', 'xp', 'stiker','quotes', 'admin', 'grup', 'premium', 'internet', 'anonymous', 'nulis', 'downloader', 'tools','anime','videomaker', 'fun', 'database', 'audio','info','owner']
+  if (!arrayMenu.includes(teks)) teks = '404'
+  if (teks == 'all') tags = {
+    'main': 'Main',
+    'game': 'Game',
+    'xp': 'Exp & Limit',
+    'anime': 'Anime',
+    'sticker': 'Stiker',
+    'shell': 'Magic Shells',
+    'quotes': 'Quotes',
+    'admin': `Admin ${global.opts['restrict'] ? '' : '(Disabled)'}`,
+    'group': 'Grup',
+    'premium': 'Premium',
+    'internet': 'Internet',
+    'anonymous': 'Anonymous Chat',
+    'nulis': 'MagerNulis & Logo',
+    'downloader': 'Downloader',
+    'tools': 'Tools',
+    'Video Maker':'videomaker',
+    'fun': 'Fun',
+    'database': 'Database',
+    'vote': 'Voting',
+    'audio': 'Voice Changer',
+    'info': 'Info',
+    '': 'No Category',
+  }
+  if (teks == 'game') tags = {
+    'game': 'Game'
+  }
+  if (teks == 'xp') tags = {
+    'xp': 'Exp & Limit'
+  }
+  if (teks == 'stiker') tags = {
+    'sticker': 'Stiker'
+  }
+  if (teks == 'shell') tags = {
+    'shell': 'Magic Shells'
+  }
+  if (teks == 'quotes') tags = {
+    'quotes': 'Quotes'
+  }
+  if (teks == 'admin') tags = {
+    'admin': `Admin ${global.opts['restrict'] ? '' : '(Disabled)'}`
+  }
+  if (teks == 'grup') tags = {
+    'group': 'Grup'
+  }
+  if (teks == 'premium') tags = {
+    'premium': 'Premium'
+  }
+  if (teks == 'videomaker') tags = {
+    'videomaker': 'Video Maker'
+  }
+  if (teks == 'internet') tags = {
+    'internet': 'Internet'
+  }
+  if (teks == 'anonymous') tags = {
+    'anonymous': 'Anonymous Chat'
+  }
+  if (teks == 'nulis') tags = {
+    'nulis': 'MagerNulis & Logo'
+  }
+  if (teks == 'downloader') tags = {
+    'downloader': 'Downloader'
+  }
+  if (teks == 'tools') tags = {
+    'tools': 'Tools'
+  }
+  if (teks == 'fun') tags = {
+    'fun': 'Fun'
+  }
+  if (teks == 'anime') tags = {
+    'anime': 'Anime'
+  }
+  if (teks == 'database') tags = {
+    'database': 'Database'
+  }
+  if (teks == 'audio') tags = {
+    'audio': 'Voice Changer'
+  }
+  if (teks == 'vote') tags = {
+    'vote': 'Voting',
+    'absen': 'Absen'
+  }
+  if (teks == 'owner') tags = {
+    'owner': 'Owner',
+    'host': 'Host',
+    'advanced': 'Advanced'
+  }
 
-Amazon.addCommand({pattern: 'menu', fromMe: tk, desc: Lang.MENU}, (async (message, match) => {
-    var time = new Date().toLocaleString('SI', { timeZone: Config.TIME_ZONE }).split(' ')[1]
 
-    var wish = ''
-     
-    var eva = ''
 
-    var auto_bio = ''
+  try {
+    let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
+    let { exp, limit, level, role, registered } = global.db.data.users[m.sender]
+    let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+    let name = registered ? global.db.data.users[m.sender].name : conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'en'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+      return {
+        help: Array.isArray(plugin.help) ? plugin.help : [plugin.help],
+        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+        prefix: 'customPrefix' in plugin,
+        limit: plugin.limit,
+        premium: plugin.premium,
+        enabled: !plugin.disabled,
+      }
+    })
+    if (teks == '404') {
+      return conn.relayWAMessage(conn.prepareMessageFromContent(m.chat, {
+        "listMessage": {
+          "title": `HI,
+╭━━━━━━ᆫ ᴍᴇɴᴜ ᄀ━━━━━━
+┏━━━━━━━━━━━━━━━━━━━━━━━━━
+┃   *DARK MAX-MD WHATSAPP BOT*
+┃
+┃🌹SELECT YOUR COMMAND LIST🌹
+┗━━━━━━━━━━━━━━━━━━━━━━━━━
+ \n`
+          
+           .trim(),
+           "description": "©ᴅᴀʀᴋ ᴍᴀx ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ",
+          "buttonText": "SELECT",
+          "listType": "SINGLE_SELECT",
+          "sections": [
+            {
+              "rows": [
+                   {
+                  "title": "MAIN COMMAND LIST",
+                  "description": "",
+                  "rowId": ".main"
 
-    var language = ''
+                }, {
+                  "title": "DOWNLOAD COMMAND LIST",
+                  "description": "",
+                  "rowId": ".downcmd"
 
-if (hrs < 12) wish = '*ᴳᴼᴼᴰ ᴹᴼᴿᴺᴵᴺᴳ ⛅*'
-if (hrs >= 12 && hrs <= 17) wish = '*ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 🌞*'
-if (hrs >= 17 && hrs <= 19) wish = '*ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌥*'
-if (hrs >= 19 && hrs <= 24) wish = '*ɢᴏᴏᴅ ɴɪɢʜᴛ 🌙*'
+                },  {
+                  "title": "MEDIA COMMAND LIST",
+                  "description": "",
+                  "rowId": ".mediacmd"
 
-    var respoimage = await axios.get(`${Config.MENU_LOGO}`, { responseType: 'arraybuffer' })
-    await message.sendMessage (Buffer.from (respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `        
+                },{
+                  "title": "TTP COMMAND LIST",
+                  "description": "",
+                  "rowId": ".? stiker"
+                }, { 
+                "title": "ANIME LOGO LIST",
+                "description": "",
+                "rowId": ".? anime"
+              },  {
+                  "title": "ADMIN COMMAND LIST",
+                  "description": "",
+                  "rowId": ".? quote"
+                }, {
+                  "title": "LOGO COMMAND LIST",
+                  "description": "",
+                  "rowId": ".textimg"
+                }, {
+                  "title": "BOOK COMMAND LIST",
+                  "description": "",
+                  "rowId": ".bookcmd"
+                }, {
+                  "title": "IMAGE COMMAND LIST",
+                  "description": "",
+                  "rowId": ".imgcmd"
+                }, {
+                  "title": "FUN COMMAND LIST",
+                  "description": "",
+                  "rowId": ".fun"
+                }, {
+                  "title": "VOICE CHANGER COMMAND",
+                  "description": "",
+                  "rowId": ".voice"
+                },
+              ]
+            }
+          ], "contextInfo": {
+            "stanzaId": m.key.id,
+            "participant": m.sender,
+            "quotedMessage": m.message
+          }
+        }
+      }, {}), { waitForAck: true })
+    }
+    // use this if you use business whatsapp
+    //   throw `
+    // ┌〔 LIST MENU 〕
+    // ├ ${_p + command} all
+    // ├ ${_p + command} game
+    // ├ ${_p + command} xp
+    // ├ ${_p + command} stiker
+    // ├ ${_p + command} quotes
+    // ├ ${_p + command} admin
+    // ├ ${_p + command} group
+    // ├ ${_p + command} premium
+    // ├ ${_p + command} internet
+    // ├ ${_p + command} anonymous
+    // ├ ${_p + command} nulis
+    // ├ ${_p + command} downloader
+    // ├ ${_p + command} tools
+    // ├ ${_p + command} fun
+    // ├ ${_p + command} database
+    // ├ ${_p + command} vote
+    // ├ ${_p + command} quran
+    // ├ ${_p + command} audio
+    // ├ ${_p + command} jadibot
+    // ├ ${_p + command} info
+    // ├ ${_p + command} owner
+    // └────  
+    //     `.trim()
+    let groups = {}
+    for (let tag in tags) {
+      groups[tag] = []
+      for (let plugin of help)
+        if (plugin.tags && plugin.tags.includes(tag))
+          if (plugin.help) groups[tag].push(plugin)
+      // for (let tag of plugin.tags)
+      //   if (!(tag in tags)) tags[tag] = tag
+    }
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == global.conn.user.jid ? '' : `Presented by https://wa.me/${global.conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let _text = [
+      before,
+      ...Object.keys(tags).map(tag => {
+        return header.replace(/%category/g, tags[tag]) + '\n' + [
+          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+            return menu.help.map(help => {
+              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+                .replace(/%islimit/g, menu.limit ? '(Limit)' : '')
+                .replace(/%isPremium/g, menu.premium ? '(Premium)' : '')
+                .trim()
+            }).join('\n')
+          }),
+          footer
+        ].join('\n')
+      }),
+      after
+    ].join('\n')
+    text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    let replace = {
+      '%': '%',
+      p: _p, uptime, muptime,
+      me: conn.user.name,
+      npmname: package.name,
+      npmdesc: package.description,
+      version: package.version,
+      exp: exp - min,
+      maxexp: xp,
+      totalexp: exp,
+      xp4levelup: max - exp <= 0 ? `Ready for *${_p}levelup*` : `${max - exp} More XP for levelup`,
+      github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
+      level, limit, name, weton, week, date, time, totalreg, rtotalreg, role,
+      readmore: readMore
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    await conn.send2ButtonImg(m.chat, await (await fetch("https://raw.githubusercontent.com/Neeraj-x0/Millie/main/Millie/mills.jpeg")).buffer(), text.trim(), '© ᴍɪʟʟɪᴇ ©', 'Owner Bot', '.owner', 'All Commands', '.listt', m)
+  } catch (e) {
+    conn.reply(m.chat, 'Sorry, Try Again', m)
+    throw e
+  }
+}
+handler.help = ['cmd', '.']
+handler.tags = ['main']
+handler.command = /^(list|menu|help|millie|bot|\?)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
 
- *╭─「 ᴄᴏᴍᴍᴀɴᴅꜱ ᴘᴀɴᴇʟ」*
- *│*
- *│ᴛʏᴘᴇ: .`+Config.PANEL_COMMAND+` for command panel*
- *├──────────●►*
- *│🥳HEY,*  `+wish+`
- *│👤Name:* `+Config.BOT_NAME+`
- *│💎Role:* Developer
- *│⏰Time:* `+time+`
- *│📡Server:* Digital Ocean
- *│📦Frameworks:* Nodejs
- *│👾Working As*
- *│🤖 `+Config.WORKTYPE+`*
- *│ ǫᴜᴇᴇɴ ᴀʟᴇxᴀ 2021*
- *╰───────────●►*
+handler.admin = false
+handler.botAdmin = false
 
- *🙇‍♂️Hey! buddy,*
-      *You can see alexa command panel, Click the menu button and Select your choice.*
- 
- *ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ǫᴜᴇᴇɴ ᴀʟᴇxᴀ*
-`}) 
 
- }));
+module.exports = handler
+
+const more = String.fromCharCode(1)
+const readMore = more.repeat(1)
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+}
+function ucapan() {
+  const time = moment.tz('Asia/Colombo').format('HH')
+  res = "Good Morning 🌄"
+  if (time >= 4) {
+    res = "Good Morning 🌞"
+  }
+  if (time > 10) {
+    res = "Good afternoon 🌅"
+  }
+  if (time >= 15) {
+    res = "Good Evening 🌆"
+  }
+  if (time >= 18) {
+    res = "Good Night 🌌"
+      }
+  return res
+}
